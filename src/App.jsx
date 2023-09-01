d4dcf4b6655e4264db1b82bf468e7ef4f1f05343
Irene Wang <ireneziwang@gmail.com>
2023-09-01 15:21:16 -0700

    add logo folder

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ export default function App() {
                       </button>
 
                       {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
+                      {/* <Menu as="div" className="relative ml-3">
                         <div>
                           <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="absolute -inset-1.5" />
@@ -117,7 +117,7 @@ export default function App() {
                             ))}
                           </Menu.Items>
                         </Transition>
-                      </Menu>
+                      </Menu> */}
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
