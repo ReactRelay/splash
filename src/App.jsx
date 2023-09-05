7e681477f06ccfb7a6e1674f538f9b3258151356
Irene Wang <ireneziwang@gmail.com>
2023-09-05 11:58:24 -0700

    add functionality to open github link

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ export default function Example() {
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">ReactRelay</span>
               <img
-                className="h-11 w-auto"
-                // src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                id="logo"
+                className="h-9 w-auto"
                 src={blackLogo}
                 alt="ReactRelay logo"
               />
@@ -108,7 +108,7 @@ export default function Example() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#4DA5CB] to-[#A6D9EF] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -135,7 +135,8 @@ export default function Example() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => window.open('https://github.com/oslabs-beta/ReactRelay', '_blank')}
+                className="rounded-md bg-[#4DA5CB] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
               >
                 Get started
               </a>
@@ -150,7 +151,7 @@ export default function Example() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#A6D9EF] to-[#4DA5CB] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      saturate: {
+        25: '.25'
+      }
+    },
   },
   plugins: [],
 }
 
+// {
+//   mytheme: {
+//     primary: '#4DA5CB',
+//     secondary: 'hover:bg-[#FFD059]',
+//     accent: '#FF7959',
+//     neutral: '#A6D9EF',
+//     'base-100': '#A6D9EF',
+//     // 'tree-background': '#22303C',
+//     info: '#73BEDE',
+//     success: '#36d399',
+//     warning: '#FFB909',
+//     error: '#f87272',
+//   },
