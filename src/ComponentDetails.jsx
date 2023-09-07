791783429419c0d9ea702094c8276f73434f169d
Alisa Jin <alisaj831@gmail.com>
2023-09-07 16:37:47 -0700

    Add updated gifs

--- src/ComponentDetails.jsx
+++ src/ComponentDetails.jsx
@@ -4,7 +4,7 @@ import {
   CircleStackIcon,
   CodeBracketIcon,
 } from '@heroicons/react/20/solid';
-import treeGif from './assets/gifs/component_tree.gif'
+import treeGif from './assets/gifs/component_details.gif'
 
 export default function FetchRequest() {
   return (
@@ -41,7 +41,7 @@ export default function FetchRequest() {
         </div>
         <div className='-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden'>
           <img
-            className='w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]'
+            className='w-[45rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[50rem]'
             src={treeGif}
             alt=''
           />
